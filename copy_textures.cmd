5f234816d7ca348448ce54aabc7bdfa8f2026bab
GhenTuong <hoaibaoce@gmail.com>
2026-07-05 20:15:30 +0700

    WIP

--- copy_textures.cmd
+++ copy_textures.cmd
@@ -1,2 +1,3 @@
 robocopy D:\_EDITOR\_unpacked\textures\veh\drone D:\CODES\MODS\GT_urban_tactics\gamedata\textures\veh\drone /MIR
+robocopy D:\_EDITOR\_unpacked\textures\wpn\urban_tactics D:\CODES\MODS\GT_urban_tactics\gamedata\textures\wpn\urban_tactics /MIR
 timeout /t 1
